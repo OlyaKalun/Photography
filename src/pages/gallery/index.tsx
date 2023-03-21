3a5b68c6a11dd95594f74b7020bb361969dc6a2a
Olya Kalun <olyakalun1998@gmail.com>
2023-03-21 13:28:15 +0200

    Connection and configuration of Seo and sitemap.

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,8 +1,17 @@
+import { HeadMetaTags } from "@/components/head-meta-tags/head-meta-tags";
 import { Layout } from "@/components/layout";
 import React from "react";
 
 const Gallery = () => {
-  return <Layout>Test Gallery</Layout>;
+  return (
+    <>
+      <HeadMetaTags
+        metaTitle="Website Gallery | Portfolio OPhotography"
+        metaDescription="Gallery【different types of filming】portraits, travels, weddings, solemn events. OPhotography"
+      />
+      <Layout>Test Gallery</Layout>
+    </>
+  );
 };
 
 export default Gallery;
